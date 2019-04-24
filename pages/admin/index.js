f020466fd1861a885dfa91f3abd39413cc3ce30a
Caio Souza <caiobsouza@hotmail.com>
2019-04-24 00:36:57 -0300

    [WIP] Dummy files to test pages routing

---
+++ pages/admin/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Link from 'next/link';
+import PodcastItem from '../../components/podcast-item';
+import Button from '../../components/button';
+
+const Pods = () => {
+  return (
+    <div className="container">
+      <div className="jumbotron">
+        <h1>Pods</h1>
+        <p>A simple app using Spotify API</p>
+        <PodcastItem title={'O SSH de Jonathas funciona'} />
+        <p>
+          <Link href="/search">
+            <a>asdfasdfasdf</a>
+          </Link>
+        </p>
+        <Button href="#" label="Use It!" type="primary" />
+      </div>
+    </div>
+  );
+};
+
+export default Pods;
